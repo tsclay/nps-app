229ff6337d26c32fe6708301befd5e74ef6ab775
Tim <tsclay9@gmail.com>
2020-06-18 18:22:06 -0400

    added MONGO_URI ; add to your .env files!

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 require('dotenv').config()
 
@@ -7,6 +8,19 @@ const MONGO_URI =
   process.env.MONGO_URI || 'mongodb://localhost:27017/nationalpark'
 
 const app = express()
+
+mongoose.connect(
+  MONGO_URI,
+  {
+    useCreateIndex: false,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => {
+    console.log(`MONGO @ ${MONGO_URI}`)
+  }
+)
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT:${PORT}`)
