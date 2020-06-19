43d823a0d964796b844f17241094ff2c2463ba11
Tim <tsclay9@gmail.com>
2020-06-18 20:14:18 -0400

    working on NPS API calls

--- public/js/app.js
+++ public/js/app.js
@@ -4,5 +4,12 @@ app.controller('mainController', [
   '$http',
   function ($http) {
     this.hello = 'hello world'
+    // GET from NPS API
+    this.getPark = (searchQuery) => {
+      $http({
+        method: 'GET',
+        url: `developer.nps.gov/api/v1/parks/?q=${searchQuery}&api_key=`
+      })
+    }
   }
 ])
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 const PORT = process.env.PORT || 3005
 const MONGO_URI =
   process.env.MONGO_URI || 'mongodb://localhost:27017/nationalpark'
+const { API_KEY } = process.env
 
 const app = express()
 
