7b9c4b0e972f9fbacc9b07b07e1ce1188ccbc699
Tim Clay <tsclay9@gmail.com>
2020-06-18 22:23:24 -0400

    added loading indicator for using when making AJAX calls

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,10 @@ app.controller('mainController', [
   '$http',
   function ($http) {
     this.hello = 'hello world'
+    this.loading = false
     // GET from NPS API
     this.getPark = (route, type, query) => {
+      this.loading = true
       $http({
         method: 'POST',
         url: '/getparks',
@@ -17,6 +19,7 @@ app.controller('mainController', [
       })
         .then(
           (response) => {
+            this.loading = false
             this.parks = response.data.data
             // console.log(response.data.data)
           },
