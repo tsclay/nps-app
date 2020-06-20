c8c8ad624e608f3a28905ac3c5537cf8ce618e4d
Tim Clay <tsclay9@gmail.com>
2020-06-20 19:23:35 -0400

    working on search form

--- public/js/states.js
+++ public/js/states.js
@@ -1,5 +1,5 @@
 const states = [
-  { name: 'Alabama', stateCode: 'AL' },
+  ({ name: 'Alabama', stateCode: 'AL' },
   { name: 'Alaska', stateCode: 'AK' },
   { name: 'Arizona', stateCode: 'AZ' },
   { name: 'Arkansas', stateCode: 'AR' },
@@ -48,62 +48,7 @@ const states = [
   { name: 'Washington', stateCode: 'WA' },
   { name: 'West Virginia', stateCode: 'WV' },
   { name: 'Wisconsin', stateCode: 'WI' },
-  { name: 'Wyoming', stateCode: 'WY' }
+  { name: 'Wyoming', stateCode: 'WY' })
 ]
 
-const app = angular.module('App', [])
-
-app.controller('mainController', [
-  '$http',
-  function ($http) {
-    this.hello = 'hello world'
-    this.loading = false
-    this.states = states
-    this.showStates = false
-    this.options = [
-      {
-        route: 'parks',
-        name: 'Parks',
-        searchTypes: [
-          { name: 'State', type: 'stateCode' },
-          { name: 'General', type: 'q' }
-        ]
-      },
-      {
-        route: 'activities/parks',
-        name: 'Things To Do'
-      }
-    ]
-    this.type = 'q'
-    this.route = null
-    this.query = null
-    // GET from NPS API
-    this.hitNPS = (route, type, query) => {
-      this.loading = true
-      $http({
-        method: 'POST',
-        url: '/getparks',
-        data: {
-          type,
-          query,
-          route
-        }
-      })
-        .then(
-          (response) => {
-            this.loading = false
-            this.parks = response.data.data
-            // console.log(response.data.data)
-          },
-          (error) => {
-            console.log('Error found: ', error)
-          }
-        )
-        .catch((error) => {
-          console.log('Catch: ', error)
-        })
-    }
-
-    // this.getPark('parks', 'stateCode', 'AL')
-  }
-])
+module.exports = states
