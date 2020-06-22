e2cd68fa66dd451c4dd9eba9dac1ac55c3924808
Tim <tsclay9@gmail.com>
2020-06-22 11:08:22 -0400

    added AJAX for user requests

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,7 @@ const app = angular.module('App', [])
 app.controller('mainController', [
   '$http',
   function ($http) {
+    const controller = this
     this.hello = 'Travel log'
     this.loading = false
     this.states = states
@@ -83,144 +84,166 @@ app.controller('mainController', [
         })
     }
 
-        // User
-    this.signup = function() {
-      $http(
-        {
-          method: 'POST',
-          url: // TODO: add route
+    // User
+  this.signup = function() {
+    $http(
+      {
+        method: 'POST',
+        url: '/nps',
+        data: {
+          username: ,// TODO: create input
+          password: ,// TODO: create input
+          email: ,// TODO: create input
+          phoneNum: ,// TODO: create input
+          firstName: ,// TODO: create input
+          lastName: ,// TODO: create input
+          premiumUser: // TODO: create input
         }
-      ).then(
-        function(response) {
-          console.log(response);
+      }
+    ).then(
+      function(response) {
+        console.log(response);
+      }
+    )
+  }
+  this.login = function() {
+    $http(
+      {
+        method: 'POST',
+        url: '/session',
+        data: {
+          username: ,// TODO: create input
+          password: // TODO: create input
         }
-      )
-    }
-
-    this.login = function() {
-      $http(
-        {
-          method: 'POST',
-          url: // TODO: add route
+      }
+    ).then(
+      function(response) {
+        console.log(response);
+      }
+    )
+  }
+  this.updateUser = function() {
+    $http(
+      {
+        method: 'PUT',
+        url: '/nps',
+        data: {
+          username: ,// TODO: create input
+          password: ,// TODO: create input
+          email: ,// TODO: create input
+          phoneNum: ,// TODO: create input
+          firstName: ,// TODO: create input
+          lastName: ,// TODO: create input
+          premiumUser: // TODO: create input
         }
-      ).then(
-        function(response) {
-          console.log(response);
+      }
+    ).then(
+      function(response) {
+        console.log(response);
+      }
+    )
+  }
+  this.logout = function() {
+    $http(
+      {
+        method: 'DELETE',
+        url: '/session'
+      }
+    ).then(
+      function(response) {
+        console.log(response);
+      }
+    )
+  }
+  // Parks
+  this.addPark = function() {
+    $http(
+      {
+        method: 'POST',
+        url: '/nps',
+        data: {
+          name: ,// TODO: create input
+          parkId: ,// TODO: create input
+          parkImage: // TODO: create input
         }
-      )
-    }
-
-    this.updateUser = function() {
-      $http(
-        {
-          method: 'PUT',
-          url: // TODO: add route
-        }
-      ).then(
-        function(response) {
-          console.log(response);
-        }
-      )
-    }
-
-    this.logout = function() {
-      $http(
-        {
-          method: 'DELETE',
-          url: // TODO: add route
-        }
-      ).then(
-        function(response) {
-          console.log(response);
-        }
-      )
-    }
-
-    // Parks
-    this.addPark = function() {
-      $http(
-        {
-          method: 'POST',
-          url: // TODO: add route
-        }
-      ).then(
-        function(response) {
-          console.log(response);
-        }
-      )
-    }
-
-    this.getAllParks = function() {
-      $http(
-        {
-          method: 'GET',
-          url: // TODO: add route
-        }
-      ).then(
-        function(response) {
-          console.log(response);
-        }
-      )
-    }
-
-    this.getSavedParks = function() {
-      $http(
-        {
-          method: 'GET',
-          url: // TODO: add route
-        }
-      ).then(
-        function(response) {
-          console.log(response);
-        }
-      )
-    }
-
-    this.updateSavedPark = function() {
-      $http(
-        {
-          method: 'PUT',
-          url: // TODO: add route
-        }
-      ).then(
-        function(response) {
-          console.log(response);
-        }
-      )
-    }
-
-    this.unsavePark = function() {
-      $http(
-        {
-          method: 'DELETE',
-          url: // TODO: add route
-        }
-      ).then(
-        function(response) {
-          console.log(response);
-        }
-      )
-    }
-
-    this.getAllParks();
-    this.getSavedParks();
-
+      }
+    ).then(
+      function(response) {
+        console.log(response);
+      }
+    )
+  }
+  //////////////////////////////////////
+  //////////////////////////////////////
+  this.getAllParks = function() {
     $http(
       {
         method: 'GET',
         url: // TODO: add route
-        // TODO: Setup url for session
       }
     ).then(
       function(response) {
-        if (true) {
-          return
+        console.log(response);
+      }
+    )
+  }
+  this.getSavedParks = function() {
+    $http(
+      {
+        method: 'GET',
+        url: // TODO: add route
+      }
+    ).then(
+      function(response) {
+        console.log(response);
+      }
+    )
+  }
+  //////////////////////////////////////
+  //////////////////////////////////////
+  this.updateSavedPark = function() {
+    $http(
+      {
+        method: 'PUT',
+        url: '/nps',
+        data: {
+          parkNotes: // TODO: create input
         }
       }
-    );
-
+    ).then(
+      function(response) {
+        console.log(response);
+      }
+    )
   }
-])
+  this.unsavePark = function() {
+    $http(
+      {
+        method: 'DELETE',
+        url: '/nps'
+      }
+    ).then(
+      function(response) {
+        console.log(response);
+      }
+    )
+  }
+  this.getAllParks();
+  this.getSavedParks();
 
+  $http(
+    {
+      method: 'GET',
+      url: // TODO: add route
+      // TODO: Setup url for session
+    }
+  ).then(
+    function(response) {
+      if (true) {
+        return
+      }
+    }
+  );
+}]);
 
 
