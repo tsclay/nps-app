d7a449c824554a93a0b90499aef84978de2c51cb
Shane <shanedubish@gmail.com>
2020-06-23 21:54:34 -0400

    add to schema

--- models/npsModel.js
+++ models/npsModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   username: { type: String, required: true },
@@ -13,9 +13,11 @@ const userSchema = mongoose.Schema({
       name: String,
       parkId: String,
       parkImage: String,
-      parkNotes: String
-    }
-  ]
-})
+      parkUrl: String,
+      parkDescription: String,
+      parkNotes: String,
+    },
+  ],
+});
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
